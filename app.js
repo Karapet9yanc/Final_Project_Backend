5e51350ee227120995eec12aed84121b9b46e431
Karapet9yanc <vkarapetiantc.exceedteam@gmail.com>
2021-12-10 18:43:46 +0300

    Code Review

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 app.use(cors());
 
 const apiRoutes = require("./src/modules/routes/routes");
 
-const url = "mongodb+srv://karapetiyanc:Q234165T@cluster0.i3yni.mongodb.net/karapetyanc?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://karapetiyanc:Q234165T@cluster0.i3yni.mongodb.net/karapetyanc?retryWrites=true&w=majority";
 mongoose.connect(url);
 
 app.use(express.json());
@@ -15,12 +16,12 @@ app.use("/", apiRoutes);
 const startApp = async () => {
   try {
     await mongoose.connect(url);
-    app.listen(4040, () => {
-      console.log('Example app listening on port 4040!');
+    app.listen(8000, () => {
+      console.log("Example app listening on port 4040!");
     });
   } catch (e) {
-    res.status(500).json(e)
-  };
+    res.status(500).json(e);
+  }
 };
 
-startApp()
+startApp();
